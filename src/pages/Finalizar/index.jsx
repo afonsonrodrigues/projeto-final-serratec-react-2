f4eb2ea986d2ed458b29ad1f80301b0a689eb42d
Afonso Nunes Rodrigues <70178659+afonsonrodrigues@users.noreply.github.com>
2021-06-27 18:39:37 -0300

    colocando a parte de finalizar

--- src/pages/Finalizar/index.jsx
+++ src/pages/Finalizar/index.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import http from "../../components/http";
 import MensagemAcerto from "../Login/MensagemAcerto";
 
-import './estilos.css';
+import "./estilos.css";
 
 const Finalizar = () => {
     const { id } = useParams();
@@ -11,7 +11,12 @@ const Finalizar = () => {
     const [pagamento, setPagamento] = useState("");
     const [pagamentos] = useState(["PIX", "BOLETO", "CREDITO", "DEBITO"]);
     const [mensagem, setMensagem] = useState("");
-    const [entrega, setEntrega] = useState('')
+    const [entrega, setEntrega] = useState("");
+
+    const formatter = new Intl.NumberFormat("pr-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
 
     const manipuladorPagamento = (evento) => {
         setPagamento(evento.target.value);
@@ -28,12 +33,7 @@ const Finalizar = () => {
                 console.log(response.data);
                 setMensagem("Compra efetuada com sucesso");
                 setEntrega(response.data);
-                {response.map((item) => (
-                    <div key={item.codigoPedido}>
-                        <p>{item.codigoPedido}</p>
-                    </div>
-                ))}
-                // verificarResultado(response.data)
+                verificarResultado(response.data);
             })
             .catch((erro) => {
                 console.log(erro);
@@ -44,21 +44,40 @@ const Finalizar = () => {
         if (entrega) {
             return (
                 <>
-                    {/* {entrega.map((item) => (
-                        <div key={item.codigoPedido}>
-                            <p>{item.codigoPedido}</p>
+                    <p>
+                        Olá, {entrega.cliente.nome}, seu pedido foi finalizado
+                        com sucesso!!!
+                    </p>
+                    <p>Número do pedido: {entrega.codigoPedido}</p>
+                    <p>Data da entega: {entrega.dataPedido}</p>
+                    {entrega.itens.map((item) => (
+                        <div className="card cardFinalizar">
+                            <div className="card-header">{item.nome}</div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">
+                                    Qtd: {item.quantidade}
+                                </li>
+                                <li className="list-group-item">
+                                    Valor unitário:{" "}
+                                    {formatter.format(item.preco)}
+                                </li>
+                            </ul>
                         </div>
-                    ))} */}
-
+                    ))}
+                    <p>{formatter.format(entrega.totalPedido)}</p>
                 </>
-            )
+            );
         }
 
         return (
             <form onSubmit={finalizarPedido}>
                 <h3>Finalizar</h3>
                 <label className="text-center mb-4">Forma de Pagamento:</label>
-                <select className="mb-4" value={pagamento} onChange={manipuladorPagamento}>
+                <select
+                    className="mb-4"
+                    value={pagamento}
+                    onChange={manipuladorPagamento}
+                >
                     <option>--------</option>
                     {pagamentos.map((pgto, indice) => (
                         <option key={indice} value={pgto}>
@@ -67,16 +86,13 @@ const Finalizar = () => {
                     ))}
                 </select>
                 <button>Finalizar</button>
-                {mensagem && <MensagemAcerto msg={mensagem} entrega={entrega} />}
+                {mensagem && (
+                    <MensagemAcerto msg={mensagem} entrega={entrega} />
+                )}
             </form>
-        )
+        );
+    };
 
-    }
-
-    return (
-        <div className="main">
-            {verificarResultado(entrega)}
-        </div>
-    );
+    return <div className="main">{verificarResultado(entrega)}</div>;
 };
 export default Finalizar;
