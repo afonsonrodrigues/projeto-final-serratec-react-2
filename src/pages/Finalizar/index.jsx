20545275868385171a703725c2c1177a3c8efc20
João Stael <joaostael@gmail.com>
2021-06-27 16:10:43 -0300

    ajuste em finalizar pedido

--- src/pages/Finalizar/index.jsx
+++ src/pages/Finalizar/index.jsx
@@ -3,12 +3,15 @@ import { useParams } from "react-router-dom";
 import http from "../../components/http";
 import MensagemAcerto from "../Login/MensagemAcerto";
 
+import './estilos.css';
+
 const Finalizar = () => {
     const { id } = useParams();
 
     const [pagamento, setPagamento] = useState("");
     const [pagamentos] = useState(["PIX", "BOLETO", "CREDITO", "DEBITO"]);
     const [mensagem, setMensagem] = useState("");
+    const [entrega, setEntrega] = useState('')
 
     const manipuladorPagamento = (evento) => {
         setPagamento(evento.target.value);
@@ -24,33 +27,41 @@ const Finalizar = () => {
             .then((response) => {
                 console.log(response);
                 setMensagem("Compra efetuada com sucesso");
-                setTimeout(() => {
-                    setMensagem("");
-                }, 4000);
+                setEntrega(response.data)
+                verificarResultado(entrega)
             })
             .catch((erro) => {
                 console.log(erro);
             });
     };
 
+    const verificarResultado = (entrega) => {
+        if (entrega) {
+            return "Foiii"
+        }
+
+        return (
+            <form onSubmit={finalizarPedido}>
+                <h3>Finalizar</h3>
+                <label className="text-center mb-4">Forma de Pagamento:</label>
+                <select className="mb-4" value={pagamento} onChange={manipuladorPagamento}>
+                    <option>--------</option>
+                    {pagamentos.map((pgto, indice) => (
+                        <option key={indice} value={pgto}>
+                            {pgto}
+                        </option>
+                    ))}
+                </select>
+                <button>Finalizar</button>
+                {mensagem && <MensagemAcerto msg={mensagem} entrega={entrega}/>}
+            </form>
+        )
+
+    }
+
     return (
-        <div>
-            <h1>Finalizar</h1>
-            <div>
-                {mensagem && <MensagemAcerto msg={mensagem} />}
-                <form onSubmit={finalizarPedido}>
-                    <label>Forma de Pagamento:</label>
-                    <select value={pagamento} onChange={manipuladorPagamento}>
-                        <option>--------</option>
-                        {pagamentos.map((pgto, indice) => (
-                            <option key={indice} value={pgto}>
-                                {pgto}
-                            </option>
-                        ))}
-                    </select>
-                    <button>Finalizar</button>
-                </form>
-            </div>
+        <div className="main">
+            {verificarResultado(entrega)}
         </div>
     );
 };
---
+++ src/pages/PedidoFinalizado/index.jsx
@@ -0,0 +1,12 @@
+const PedidoFinalizado = () => {
+    return(
+        <div className="main">
+            <div className="card">
+                <h4>Pedido finalizado com sucesso</h4>
+                
+            </div>
+        </div>
+    ) 
+}
+
+export default PedidoFinalizado;
